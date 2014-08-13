fc049263bc47035c618d5ab18679f088d336a917
Gawain Lynch <gawain.lynch@gmail.com>
2014-08-13 11:52:52 +0100

    Add database check

--- Extension.php
+++ Extension.php
@@ -16,6 +16,48 @@ class Extension extends \Bolt\BaseExtension
 
     public function initialize()
     {
-        $hybridauth = new \Hybrid_Auth(array());
+        if ($this->app['config']->getWhichEnd() == 'backend') {
+            // Check & create database tables if required
+            $this->dbCheck();
+        }
+
+        if ($this->app['config']->getWhichEnd() == 'frontend') {
+            $hybridauth = new \Hybrid_Auth(array());
+        }
+    }
+
+    private function dbCheck()
+    {
+        // Set up database schema
+        $table_prefix = $this->app['config']->get('general/database/prefix', "bolt_");
+
+        // CREATE TABLE 'bolt_visitors'
+        $this->app['integritychecker']->registerExtensionTable(
+            function ($schema) use ($table_prefix) {
+                $table = $schema->createTable($table_prefix . "visitors");
+                $table->addColumn("id", "integer", array('autoincrement' => true));
+                $table->setPrimaryKey(array("id"));
+                $table->addColumn("username", "string", array("length" => 64));
+                $table->addColumn("provider", "string", array("length" => 64));
+                $table->addColumn("providerdata", "text");
+                $table->addColumn("apptoken", "string", array("length" => 64, 'notnull' => false));
+                return $table;
+            }
+        );
+
+        // CREATE TABLE 'bolt_visitors_sessions'
+        $this->app['integritychecker']->registerExtensionTable(
+            function ($schema) use ($table_prefix) {
+                $table = $schema->createTable($table_prefix . "visitors_sessions");
+                $table->addColumn("id", "integer", array('autoincrement' => true));
+                $table->setPrimaryKey(array("id"));
+                $table->addColumn("visitor_id", "integer");
+                $table->addColumn("sessiontoken", "string", array('length' => 64));
+                $table->addColumn("lastseen", "datetime");
+                $table->addIndex(array("visitor_id"));
+                $table->addIndex(array("sessiontoken"));
+                return $table;
+            }
+        );
     }
 }
