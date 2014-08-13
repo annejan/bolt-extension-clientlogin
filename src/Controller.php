304c57e94d05274973d98de04d2e90c1cd938adf
Gawain Lynch <gawain.lynch@gmail.com>
2014-08-13 12:08:58 +0100

    Populate controller class

--- src/Controller.php
+++ src/Controller.php
@@ -1,1 +1,51 @@
 <?php
+
+namespace SocialLogin;
+
+/**
+ * Membership profiles
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Controller
+{
+    /**
+     *
+     * @param Silex\Application $app
+     * @return multitype:
+     */
+    public function getMemberRoot(Silex\Application $app = null)
+    {
+        return array();
+    }
+
+    /**
+     *
+     * @param Silex\Application $app
+     * @return multitype:
+     */
+    public function getMemberLogin(Silex\Application $app = null)
+    {
+        return array();
+    }
+
+    /**
+     *
+     * @param Silex\Application $app
+     * @return multitype:
+     */
+    public function getMemberLogout(Silex\Application $app = null)
+    {
+        return array();
+    }
+
+    /**
+     *
+     * @param Silex\Application $app
+     * @return multitype:
+     */
+    public function getMemberEndpoint(Silex\Application $app = null)
+    {
+        return array();
+    }
+}
