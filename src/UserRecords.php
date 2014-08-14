41f478d03a2a87699d8cb6c15deb6cc0e3603fc6
Gawain Lynch <gawain.lynch@gmail.com>
2014-08-14 14:19:10 +0100

    Start user query handling

--- src/UserRecords.php
+++ src/UserRecords.php
@@ -25,9 +25,32 @@ class UserRecords
         $this->config = $config;
     }
 
-    public function getUserRecord()
+    public function getUserRecord($username, $provider)
     {
+        // Get the user record
+        $query = "SELECT * FROM " . $this->getTableNameUser() .
+                 " WHERE username = :username AND provider = :provider";
+        $map = array(
+            ':username' => $username,
+            ':provider' => $provider
+        );
+        $user = $this->db->fetchAssoc($query, $map);
 
+        if ($user['id']) {
+            // Get the assocaited session
+            $query = "SELECT * FROM " . $this->getTableNameSession() .
+                     " WHERE userid = :userid ORDER BY lastseen DESC";
+            $map = array(
+                ':userid' => $user['id']
+            );
+            $sess = $this->db->fetchAssoc($query, $map);
+
+            // Map relative parts to returnable array
+            $user['sessiontoken'] = (isset($sess['sessiontoken']) ? $sess['sessiontoken'] : '');
+            $user['lastseen'] = (isset($sess['lastseen']) ? $sess['lastseen'] : '');
+        }
+
+        return $user;
     }
 
     public function setUserRecord()
@@ -35,4 +58,37 @@ class UserRecords
 
     }
 
+    /**
+     * Get the name of the user record table
+     *
+     * @return string
+     */
+    private function getTableNameUser()
+    {
+        $this->prefix = $this->config->get('general/database/prefix', "bolt_");
+
+        // Make sure prefix ends in '_'. Prefixes without '_' are lame..
+        if ($this->prefix[ strlen($this->prefix)-1 ] != "_") {
+            $this->prefix .= "_";
+        }
+
+        return $this->prefix . 'sociallogin_users';
+    }
+
+    /**
+     * Get the name of the user session table
+     *
+     * @return string
+     */
+    private function getTableNameSession()
+    {
+        $this->prefix = $this->config->get('general/database/prefix', "bolt_");
+
+        // Make sure prefix ends in '_'. Prefixes without '_' are lame..
+        if ($this->prefix[ strlen($this->prefix)-1 ] != "_") {
+            $this->prefix .= "_";
+        }
+
+        return $this->prefix . 'sociallogin_sessions';
+    }
 }
