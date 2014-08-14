ba68b6ed09a7537e8eda1b99290312adacd76054
Gawain Lynch <gawain.lynch@gmail.com>
2014-08-14 13:15:16 +0100

    Basics of session auth

--- src/Controller.php
+++ src/Controller.php
@@ -15,13 +15,54 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 class Controller
 {
     /**
+     * @var Silex\Application
+     */
+    private $app;
+
+    /**
+     * @var Extension config array
+     */
+    private $config;
+
+    public function __construct(Silex\Application $app, $config)
+    {
+        $this->app = $app;
+        $this->config = $config;
+    }
+
+    /**
      *
      * @param \Silex\Application $app
      * @return multitype:
      */
     public function getAuthenticationLogin(\Silex\Application $app, Request $request)
     {
-        return array();
+        $auth = new Session($this->app, $this->config);
+
+        if ($auth->isLoggedIn()) {
+            // User is already logged in, return them... somewhere
+            $this->doRedirect();
+        } else {
+            $provider = $request->query->get('provider');
+
+            if ($provider) {
+                //
+                $result = $auth->doLogin($provider);
+
+                if ($result['result']) {
+                    // Login done, redirect
+                    $this->doRedirect();
+                } else {
+                    return $result['error'];
+                }
+
+            } else {
+            // This shouldn't happen, just die here
+                return '<pre>Provider not given</pre>';
+            }
+        }
+
+        return $this->page($title, $markup);
     }
 
     /**
@@ -42,5 +83,23 @@ class Controller
     public function getAuthenticationEndpoint(\Silex\Application $app, Request $request)
     {
         return array();
+    }
+
+    /**
+     * Do the best redirect we can
+     *
+     * @param \Silex\Application $app
+     */
+    private function doRedirect(\Silex\Application $app)
+    {
+        $returnpage = $app['request']->headers->get('referer');
+        $returnpage = str_replace($app['paths']['hosturl'], '', $returnpage);
+
+        if($returnpage) {
+            simpleredirect($returnpage);
+            exit;
+        } else {
+            return redirect('homepage');
+        }
     }
 }
---
+++ src/Session.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace SocialLogin;
+
+/**
+ * Authentication class
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Session
+{
+    /**
+     * @var Boolean that is true if session is active
+     */
+    private $isLoggedIn = false;
+
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Test if the session is authenticated
+     *
+     * @return boolean
+     */
+    public function isLoggedIn()
+    {
+        return $this->isLoggedIn;
+    }
+
+    /**
+     * Do login authentication
+     *
+     * @param string Provider name to authenticate with
+     */
+    public function doLogin($provider)
+    {
+        if ($this->doCheckLogin()) {
+            return true;
+        } else {
+
+            // Attempt a HybridAuth login
+            try {
+
+                // get the type early - because we might need to enable it
+                if (isset($this->config['providers'][$provider]['type'])) {
+                    $providertype = $this->config['providers'][$provider]['type'];
+                } else {
+                    $providertype = $provider;
+                }
+
+                // enable OpenID
+                if($providertype == 'OpenID' && $this->config['providers'][$provider]['enabled'] == true) {
+                    $this->config['providers']['OpenID']['enabled'] = true;
+                }
+
+                // Initialize the authentication with the modified config
+                $hybridauth = new \Hybrid_Auth($this->config);
+
+                $provideroptions = array();
+                if ($providertype=='OpenID' && !empty($this->config['providers'][$provider]['openid_identifier'])) {
+                    // Try to authenticate with the selected OpenID provider
+                    $providerurl = $this->config['providers'][$provider]['openid_identifier'];
+                    $provideroptions["openid_identifier"] = $providerurl;
+                }
+                // Try to authenticate with the selected provider
+                $adapter = $hybridauth->authenticate($providertype, $provideroptions);
+
+                // then grab the user profile
+                $user_profile = $adapter->getUserProfile();
+
+                if($user_profile) {
+                    $records = new UserRecords($this->app, $this->config);
+                }
+
+
+
+
+
+
+
+
+
+            } catch(Exception $e) {
+                $html =  "<pre>Error: please try again!<pre><br>";
+                $html .= "<pre>Original error message: " . $e->getMessage() . "<pre>";
+
+                return array('result' => false, 'error' => $html);
+            }
+
+            // Success
+            $this->isLoggedIn = true;
+
+            return array('result' => true, 'error' => '');
+        }
+    }
+
+    /**
+     * Logout session
+     */
+    public function doLogout()
+    {
+        $this->isLoggedIn = false;
+    }
+
+    /**
+     *
+     */
+    public function doCheckLogin()
+    {
+        //$this->isLoggedIn = true;
+    }
+}
