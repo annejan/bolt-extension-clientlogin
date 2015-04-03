6acc6e3034af1cc2153605d1a90a0c3b6f6a73f8
Gawain Lynch <gawain.lynch@gmail.com>
2015-04-03 16:16:37 +0100

    Add a ProviderException class

---
+++ src/Exception/ProviderException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bolt\Extension\Bolt\ClientLogin\Exception;
+
+class ProviderException extends \Exception
+{
+    public function __construct($message = null, $code = 0, \Exception $previous = null)
+    {
+    }
+}
