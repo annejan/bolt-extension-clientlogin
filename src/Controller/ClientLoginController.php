19e22b722952de7986c91c37e8cba8786171b740
Gawain Lynch <gawain.lynch@gmail.com>
2015-04-04 10:07:08 +0100

    Handle OAuth callback in the controller

--- src/Controller/ClientLoginController.php
+++ src/Controller/ClientLoginController.php
@@ -17,6 +17,9 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class ClientLoginController implements ControllerProviderInterface
 {
+    /** @var string */
+    const TOKENNAME = 'bolt_clientlogin_redirect';
+
     /** @var array Extension config */
     private $config;
 
@@ -29,9 +32,7 @@ class ClientLoginController implements ControllerProviderInterface
     {
         $this->config = $app[Extension::CONTAINER]->config;
 
-        /**
-         * @var $ctr \Silex\ControllerCollection
-         */
+        /** @var $ctr \Silex\ControllerCollection */
         $ctr = $app['controllers_factory'];
 
         // Member login
@@ -58,10 +59,12 @@ class ClientLoginController implements ControllerProviderInterface
      * @param \Silex\Application $app
      * @param Request            $request
      *
-     * @return multitype:
+     * @return Response
      */
     public function authenticationLogin(Application $app, Request $request)
     {
+        $this->setRedirectUrl($app);
+
         return $app['clientlogin.session']->doLogin($request);
     }
 
@@ -70,9 +73,13 @@ class ClientLoginController implements ControllerProviderInterface
      *
      * @param \Silex\Application $app
      * @param Request            $request
+     *
+     * @return RedirectResponse
      */
     public function authenticationLogout(Application $app, Request $request)
     {
+        $this->clearRedirectUrl($app);
+
         $app['clientlogin.session']->doLogout();
 
         // Logout done, redirect
@@ -80,14 +87,70 @@ class ClientLoginController implements ControllerProviderInterface
     }
 
     /**
-     * HybridAuth endpoint — passes all login requests to HybridAuth
+     * OAuth endpoint
      *
      * @param \Silex\Application $app
+     * @param Request            $request
+     *
+     * @return Response
      */
     public function authenticationEndpoint(Application $app, Request $request)
     {
-        \Hybrid_Endpoint::process();
+        $url = $this->getRedirectUrl($app);
+        $this->clearRedirectUrl($app);
 
-        return new Response('', Response::HTTP_OK);
+        // Check 'code' isn't empty
+        if (empty($app['request']->get('code'))) {
+            return new Response('Kittens!', Response::HTTP_FORBIDDEN);
+        }
+
+        // Given state must match previously stored one to mitigate CSRF attack
+        if (!$app['clientlogin.session']->checkStateToken($request->get('state'))) {
+            $app['clientlogin.session']->clearStateToken();
+
+            return new Response('Kittens!', Response::HTTP_FORBIDDEN);
+        }
+
+        return $app['clientlogin.session']->doCheckLoginOAuth($request, $url);
+    }
+
+    /**
+     * Save the redirect URL
+     *
+     * @param \Silex\Application $app
+     */
+    private function setRedirectUrl(Application $app)
+    {
+        $returnpage = $app['request']->get('redirect');
+
+        if ($returnpage) {
+            $returnpage = str_replace($app['resources']->getUrl('hosturl'), '', $returnpage);
+        } else {
+            $returnpage = $this->app['resources']->getUrl('hosturl');
+        }
+
+        $app['session']->set(self::TOKENNAME, $returnpage);
+    }
+
+    /**
+     * Get the redirect URL
+     *
+     * @param \Silex\Application $app
+     *
+     * @return string
+     */
+    private function getRedirectUrl($app)
+    {
+        return $app['session']->get(self::TOKENNAME);
+    }
+
+    /**
+     * Clear the redirect URL
+     *
+     * @param \Silex\Application $app
+     */
+    private function clearRedirectUrl($app)
+    {
+        $app['session']->remove(self::TOKENNAME);
     }
 }
