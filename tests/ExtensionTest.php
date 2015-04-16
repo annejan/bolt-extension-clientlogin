0213d38708a8a6aa170ec443c9411dcfede777b4
Gawain Lynch <gawain.lynch@gmail.com>
2015-04-16 20:51:10 +0100

    [Tests] Extension::cronDaily()

--- tests/ExtensionTest.php
+++ tests/ExtensionTest.php
@@ -2,8 +2,12 @@
 
 namespace Bolt\Extension\Bolt\ClientLogin\Tests;
 
-use Bolt\Tests\BoltUnitTest;
+use Bolt\Nut\CronRunner;
+use Bolt\Nut\DatabaseRepair;
 use Bolt\Extension\Bolt\ClientLogin\Extension;
+use Bolt\Tests\BoltUnitTest;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Tester\CommandTester;
 
 /**
  * Ensure that ClientLogin loads correctly.
@@ -11,13 +15,39 @@ use Bolt\Extension\Bolt\ClientLogin\Extension;
  */
 class ExtensionTest extends BoltUnitTest
 {
+    public function setup()
+    {
+        $this->resetDb();
+
+        $app = $this->getApp();
+        $command = new DatabaseRepair($app);
+        $tester = new CommandTester($command);
+        $tester->execute([]);
+    }
+
     public function testExtensionRegister()
     {
         $app = $this->getApp();
         $extension = new Extension($app);
-        $app['extensions']->register( $extension );
+        $app['extensions']->register($extension);
         $name = $extension->getName();
         $this->assertSame($name, 'ClientLogin');
         $this->assertSame($extension, $app["extensions.$name"]);
+    }
+
+    public function testCronDaily()
+    {
+        $app = $this->getApp();
+        $extension = new Extension($app);
+        $app['extensions']->register($extension);
+        $name = $extension->getName();
+
+        $app["extensions.$name"]->initialize();
+
+        $command = new CronRunner($app);
+        $tester = new CommandTester($command);
+        $tester->execute([]);
+        $result = $tester->getDisplay();
+        $this->assertRegExp('/ClientLogin: Clearing old sessions/', $result);
     }
 }
