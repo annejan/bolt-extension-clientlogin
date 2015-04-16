2500066383db1f33d6536c40b5efe97026b3e827
Gawain Lynch <gawain.lynch@gmail.com>
2015-04-16 22:50:00 +0100

    [Tests] Layout the Session tests

---
+++ tests/SessionTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Bolt\Extension\Bolt\ClientLogin\Tests;
+
+use Bolt\Extension\Bolt\ClientLogin\Extension;
+use Bolt\Tests\BoltUnitTest;
+
+/**
+ *
+ */
+class SessionTest extends BoltUnitTest
+{
+    public function testGetResponse()
+    {
+
+    }
+
+    public function testSetResponse()
+    {
+
+    }
+
+    public function testDoLogin()
+    {
+
+    }
+
+    public function testLoginCheckOAuth()
+    {
+
+    }
+
+    public function testLogout()
+    {
+
+    }
+
+    public function testIsLoggedIn()
+    {
+
+    }
+    public function testGetToken()
+    {
+
+    }
+    public function testRemoveToken()
+    {
+
+    }
+
+    public function testCheckStateToken()
+    {
+
+    }
+
+}
