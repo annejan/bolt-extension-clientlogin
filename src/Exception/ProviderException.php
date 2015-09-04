c3e95bb377adcae90f25feab0249ca45baf7b181
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-04 11:30:44 +0100

    Don't override parent constructor in ProviderException

--- src/Exception/ProviderException.php
+++ src/Exception/ProviderException.php
@@ -4,7 +4,5 @@ namespace Bolt\Extension\Bolt\ClientLogin\Exception;
 
 class ProviderException extends \Exception
 {
-    public function __construct($message = null, $code = 0, \Exception $previous = null)
-    {
-    }
+    const INVALID_PROVIDER = 'Invalid provider.';
 }
