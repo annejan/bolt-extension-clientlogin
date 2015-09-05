4ce649635e83e843e8822260fd3d1c8849aa4f5f
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-05 14:49:41 +0100

    Singleton class for authorisation types

---
+++ src/Authorisation/Types.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Bolt\Extension\Bolt\ClientLogin\Authorisation;
+
+/**
+ * Authorisation type constants.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Types
+{
+    /**
+     * We are the Singleton. You will be assimilated. Resistance is futile.
+     */
+    private function __construct()
+    {
+    }
+
+    const AUTH_PASSWORD = 'password';
+    const AUTH_OAUTH1 = 'oauth1';
+    const AUTH_OAUTH2 = 'oauth2';
+}
