4962334c9037b033c686086c6f86d78409b06bbc
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-05 11:28:05 +0100

    Session query class

---
+++ src/Database/Query/Session.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Bolt\Extension\Bolt\ClientLogin\Database\Query;
+
+/**
+ * Client session table queries.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Session extends QueryBase
+{
+}
