de42eb86e03e724ea95120186e8ad31ff073f308
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-05 11:27:56 +0100

    Profile query class

---
+++ src/Database/Query/Profile.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Bolt\Extension\Bolt\ClientLogin\Database\Query;
+
+/**
+ * Client profile table queries.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Profile extends QueryBase
+{
+}
