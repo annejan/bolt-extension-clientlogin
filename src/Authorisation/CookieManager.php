11c01a75b60e10ce177a714b6071ecaa09773b65
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-08 07:32:22 +0100

    Authentication cookie manager

---
+++ src/Authorisation/CookieManager.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Bolt\Extension\Bolt\ClientLogin\Authorisation;
+
+use Bolt\Extension\Bolt\ClientLogin\Database\Records;
+use RandomLib\Generator;
+use Symfony\Component\HttpFoundation\Cookie;
+
+/**
+ * Cookie manager class.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class CookieManager
+{
+    /** @var \RandomLib\Generator */
+    protected $random;
+
+    /**
+     * Constructor.
+     *
+     * @param Records   $records
+     * @param Generator $random
+     */
+    public function __construct(Records $records, Generator $random)
+    {
+        $this->records = $records;
+        $this->random = $random;
+    }
+
+    /**
+     * Create an authentication cookie.
+     *
+     * @param Token $token
+     *
+     * @return \Symfony\Component\HttpFoundation\Cookie
+     */
+    public function create(Token $token)
+    {
+        $value = $this->random->generateString(32);
+        $cookie = new Cookie('bolt_clientlogin', $value, $token->getExpires(), '/', null, false, false);
+        $this->commit($cookie, $token);
+
+        return $cookie;
+    }
+
+    /**
+     * Update an authentication cookie.
+     *
+     * @param Cookie $cookie
+     * @param Token $token
+     *
+     * @return \Symfony\Component\HttpFoundation\Cookie
+     */
+    public function update(Cookie $cookie, Token $token)
+    {
+        $cookie = new Cookie('bolt_clientlogin', (string) $cookie, $token->getExpires(), '/', null, false, false);
+        $this->commit($cookie, $token);
+
+        return $cookie;
+    }
+
+    /**
+     * Insert/update a session record for an authentication cookie.
+     *
+     * @param Cookie $cookie
+     * @param Token $token
+     *
+     * @return integer|null
+     */
+    protected function commit(Cookie $cookie, Token $token)
+    {
+        if ($session = $this->records->getSessionBySessionId((string) $cookie)) {
+            return $this->records->updateSession($session['userid'], (string) $cookie, $token);
+        } else {
+            return $this->records->insertSession($session['userid'], (string) $cookie, $token);
+        }
+    }
+}
