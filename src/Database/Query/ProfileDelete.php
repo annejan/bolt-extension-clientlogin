582ec5e946fc2224622a7c1b4192432eedd9f0b9
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-14 19:11:29 +0100

    Delete profile queries

--- src/Database/Query/ProfileDelete.php
+++ src/Database/Query/ProfileDelete.php
@@ -7,17 +7,22 @@ namespace Bolt\Extension\Bolt\ClientLogin\Database\Query;
  *
  * @author Gawain Lynch <gawain.lynch@gmail.com>
  */
-class ProfileRemove extends QueryBase
+class ProfileDelete extends QueryBase
 {
     /**
+     * @param string $provider
+     * @param string $resourceOwnerId
+     *
      * @return \Doctrine\DBAL\Query\QueryBuilder
      */
-    public function queryDelete()
+    public function queryDelete($provider, $resourceOwnerId)
     {
         return $this->getQueryBuilder()
             ->delete($this->tableName)
             ->where('provider  = :provider')
             ->andWhere('resource_owner_id  = :resource_owner_id')
+            ->setParameter(':provider', $provider)
+            ->setParameter(':resource_owner_id', $resourceOwnerId)
         ;
     }
 }
