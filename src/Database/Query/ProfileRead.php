cfa06545d3fc5d786d7c6dc785973c90334c69f7
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-14 16:34:07 +0100

    Update read queries

--- src/Database/Query/ProfileRead.php
+++ src/Database/Query/ProfileRead.php
@@ -10,42 +10,41 @@ namespace Bolt\Extension\Bolt\ClientLogin\Database\Query;
 class ProfileRead extends QueryBase
 {
     /**
+     * Query to fetch a profile by GUID.
+     *
+     * @param string $id
+     *
      * @return \Doctrine\DBAL\Query\QueryBuilder
      */
-    public function queryFetchById()
+    public function queryFetchById($id)
     {
         return $this->getQueryBuilder()
             ->select('*')
             ->from($this->tableName)
             ->where('id = :id')
             ->orderBy('lastupdate', 'DESC')
+            ->setParameter(':id', $id)
         ;
     }
 
     /**
+     * Query to fetch a profile by provider and ID.
+     *
+     * @param string $provider
+     * @param string $resourceOwnerId
+     *
      * @return \Doctrine\DBAL\Query\QueryBuilder
      */
-    public function queryFetchByAccessToken()
+    public function queryFetchByResourceOwnerId($provider, $resourceOwnerId)
     {
         return $this->getQueryBuilder()
             ->select('*')
             ->from($this->tableName)
-            ->where('access_token = :access_token')
+            ->where('provider = :provider')
+            ->andWhere('resource_owner_id = :resource_owner_id')
             ->orderBy('lastupdate', 'DESC')
-        ;
-    }
-
-    /**
-     * @return \Doctrine\DBAL\Query\QueryBuilder
-     */
-    public function queryFetchByResource()
-    {
-        return $this->getQueryBuilder()
-            ->select('*')
-            ->from($this->tableName)
-            ->where('provider  = :provider')
-            ->andWhere('resource_owner_id  = :resource_owner_id')
-            ->orderBy('lastupdate', 'DESC')
+            ->setParameter(':provider', $provider)
+            ->setParameter(':resource_owner_id', $resourceOwnerId)
         ;
     }
 }
