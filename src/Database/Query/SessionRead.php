0133abbfc955e35a227a1e5ed7373364b5e48c42
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-15 09:31:46 +0100

    Session read queries

---
+++ src/Database/Query/SessionRead.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bolt\Extension\Bolt\ClientLogin\Database\Query;
+
+/**
+ * Client session read queries.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class SessionRead extends QueryBase
+{
+    /**
+     * Fetch a session based on access token.
+     *
+     * @return \Doctrine\DBAL\Query\QueryBuilder
+     */
+    public function queryFetchByAccessToken($cookie)
+    {
+        return $this->getQueryBuilder()
+            ->select('*')
+            ->from($this->tableName)
+            ->where('token  = :token')
+            ->orderBy('expires', 'DESC')
+            ->setParameter(':token', $cookie)
+        ;
+    }
+
+    /**
+     * Fetch session records by a GUID.
+     *
+     * @param string $guid
+     *
+     * @return \Doctrine\DBAL\Query\QueryBuilder
+     */
+    public function queryFetchByGuid($guid)
+    {
+        return $this->getQueryBuilder()
+            ->select('*')
+            ->from($this->tableName)
+            ->where('guid  = :guid')
+            ->orderBy('expires', 'DESC')
+            ->setParameter(':guid', $guid)
+        ;
+    }
+}
