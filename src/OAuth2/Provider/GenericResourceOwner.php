168ed10bdaf8a9607344f5ebc90325b789bab3cb
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-17 17:50:33 +0100

    Extend the GenericProvider… for our own :evil:

---
+++ src/OAuth2/Provider/GenericResourceOwner.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\Bolt\ClientLogin\OAuth2\Provider;
+
+use League\OAuth2\Client\Provider\GithubResourceOwner as LeagueGitHubResourceOwner;
+
+/**
+ * Generic provider ResourceOwner provider extension.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class GitHubResourceOwner extends LeagueGitHubResourceOwner
+{
+}
