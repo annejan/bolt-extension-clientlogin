4042f872887696f9f0b44b419a7bb7181512f23c
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-19 15:30:22 +0100

    Fix namespaces

--- src/Response/SuccessRedirectResponse.php
+++ src/Response/SuccessRedirectResponse.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Bolt\Extension\Bolt\ClientLogin\Authorisation\Response;
+namespace Bolt\Extension\Bolt\ClientLogin\Response;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
