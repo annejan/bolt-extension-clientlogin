e30b6bb6f5287f4c5bc2546499d7adf7dbf0c3b9
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-21 10:51:37 +0100

    Local OAuth2 server service class

---
+++ src/OAuth2/Service/Server.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bolt\Extension\Bolt\ClientLogin\OAuth2\Service;
+
+/**
+ * Local OAuth server manager.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Server
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+    }
+}
