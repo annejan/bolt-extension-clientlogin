8d1f8beb57e4cfbd78f995d243f5f65a2e9270d5
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-24 12:25:15 +0100

    Events for Authroisation Server

--- src/OAuth2/AuthorisationServer/Server.php
+++ src/OAuth2/AuthorisationServer/Server.php
@@ -3,10 +3,16 @@
 namespace Bolt\Extension\Bolt\ClientLogin\OAuth2\AuthorisationServer;
 
 use Doctrine\DBAL\Driver\Connection;
+use League\Event\EventInterface;
 use League\OAuth2\Server\AuthorizationServer;
 use League\OAuth2\Server\ResourceServer;
+use League\OAuth2\Server\Exception\OAuthException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Request;
+use League\OAuth2\Server\Event\ClientAuthenticationFailedEvent;
+use League\OAuth2\Server\Event\UserAuthenticationFailedEvent;
+use League\OAuth2\Server\Event\SessionOwnerEvent;
 
 /**
  * Local OAuth server manager.
@@ -38,6 +44,15 @@ class Server
         $this->setResourceServer();
     }
 
+    public function grantAuthorisationCode(Request $request)
+    {
+        try {
+        } catch (OAuthException $e) {
+        }
+
+        return new Response();
+    }
+
     /**
      * Set up an OAuth2 Authorization Server.
      */
@@ -54,6 +69,10 @@ class Server
             $clientStorage,
             $scopeStorage
         );
+
+        $this->authorisationServer->addEventListener('error.auth.client', [$this, 'eventErrorAuthClient']);
+        $this->authorisationServer->addEventListener('error.auth.user', [$this, 'eventErrorAuthUser']);
+        $this->authorisationServer->addEventListener('session.owner', [$this, 'eventSessionOwner']);
     }
 
     /**
@@ -72,5 +91,45 @@ class Server
             $clientStorage,
             $scopeStorage
         );
+    }
+
+    /**
+     * Emitted when a client fails to authenticate.
+     *
+     * Listen to this event in order to ban clients that fail to authenticate
+     * after 'n' number of attempts
+     *
+     * @param \League\OAuth2\Server\Event\ClientAuthenticationFailedEvent
+     */
+    public function eventErrorAuthClient(ClientAuthenticationFailedEvent $event)
+    {
+        $request = $event->getRequest();
+    }
+
+    /**
+     * Emitted when a user fails to authenticate.
+     *
+     * Listen to this event in order to reset passwords or ban users that fail
+     * to authenticate after 'n' number of attempts.
+     *
+     * @param \League\OAuth2\Server\Event\UserAuthenticationFailedEvent
+     */
+    public function eventErrorAuthUser(UserAuthenticationFailedEvent $event)
+    {
+        $request = $event->getRequest();
+    }
+
+    /**
+     * Emitted when a session has been allocated an owner (for example a user or
+     * a client).
+     *
+     * You might want to use this event to dynamically associate scopes to the
+     * session depending on the users role or ACL permissions.
+     *
+     * @param \League\OAuth2\Server\Event\SessionOwnerEvent
+     */
+    public function eventSessionOwner(SessionOwnerEvent $event)
+    {
+        $session = $event->getSession();
     }
 }
